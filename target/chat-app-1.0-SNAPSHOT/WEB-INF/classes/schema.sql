b42a3cb851a4fe5f4053d2c8b6842ecf7d82e63c
sammithk <sammith@Sammiths-MacBook-Pro.local>
2023-04-12 19:52:33 +0530

    changed env name

--- target/chat-app-1.0-SNAPSHOT/WEB-INF/classes/schema.sql
+++ target/chat-app-1.0-SNAPSHOT/WEB-INF/classes/schema.sql
@@ -1,23 +1,23 @@
-DROP DATABASE IF EXISTS `chatApp`;
-CREATE DATABASE `chatApp`;
-DROP TABLE IF EXISTS `chatApp`.`user`;
-CREATE TABLE `chatApp`.`user` (
+DROP DATABASE `chatapp`;
+CREATE DATABASE `chatapp`;
+DROP TABLE IF EXISTS `chatapp`.`user`;
+CREATE TABLE `chatapp`.`user` (
 `userName` VARCHAR(255) NOT NULL,
 `password` VARCHAR(255) NOT NULL,
 `groups` INTEGER,
 `followers` INTEGER,
 PRIMARY KEY (`userName`)
 );
-DROP TABLE IF EXISTS `chatApp`.`friends`;
-CREATE TABLE `chatApp`.`friends` (
+DROP TABLE IF EXISTS `chatapp`.`friends`;
+CREATE TABLE `chatapp`.`friends` (
  `user` VARCHAR(255) NOT NULL,
  `connection` VARCHAR(255) NOT NULL,
  `status` INTEGER NOT NULL,
  `calling` INTEGER NOT NULL,
  PRIMARY KEY (`user`, `connection`)
 );
-DROP TABLE IF EXISTS `chatApp`.`groups`;
-CREATE TABLE `chatApp`.`groups` (
+DROP TABLE IF EXISTS `chatapp`.`groups`;
+CREATE TABLE `chatapp`.`groups` (
 `groupName` VARCHAR(255) NOT NULL,
 `userName` VARCHAR(255) NOT NULL,
 `status` INTEGER NOT NULL,
